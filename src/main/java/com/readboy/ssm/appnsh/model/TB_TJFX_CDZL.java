98b57a2095695cc9f098b4b5066cb4b9c943549c
PC-20151031KAQH\Administrator <jlf1997@163.com>
2018-11-06 18:38:38 +0800

    new table

---
+++ src/main/java/com/readboy/ssm/appnsh/model/TB_TJFX_CDZL.java
@@ -0,0 +1,12 @@
+package com.readboy.ssm.appnsh.model;
+
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name="TB_TJFX_CDZL")
+public class TB_TJFX_CDZL {
+
+	
+	@Id
+	private Long id;
+}
