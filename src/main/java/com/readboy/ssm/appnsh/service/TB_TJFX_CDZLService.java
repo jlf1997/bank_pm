1a3d04a2d4c6f9b28b3d10bc076fdf25ad6006fb
PC-20151031KAQH\Administrator <jlf1997@163.com>
2019-01-11 15:02:17 +0800

    增加错误处理

--- src/main/java/com/readboy/ssm/appnsh/service/TB_TJFX_CDZLService.java
+++ src/main/java/com/readboy/ssm/appnsh/service/TB_TJFX_CDZLService.java
@@ -2,20 +2,22 @@ package com.readboy.ssm.appnsh.service;
 
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Service;
 
 import com.readboy.ssm.appnsh.jpa.JdbcTemplatePageHelper;
-import com.readboy.ssm.appnsh.model.TB_TJFX_BLDKKH;
 import com.readboy.ssm.appnsh.model.TB_TJFX_CDZL;
+import com.readboy.ssm.utils.LogsUtil;
 
 @Service
 public class TB_TJFX_CDZLService {
+	
+	private static final Logger log = LoggerFactory.getLogger(TB_TJFX_CDZLService.class);
 	
 	
 	@Autowired
@@ -40,11 +42,17 @@ public class TB_TJFX_CDZLService {
 
 
 	public List<TB_TJFX_CDZL> findByYggh(String yggh) {
-		//tjrq 选最大的
-		String sql = "select * from tb_tjfx_cdzl where yggh=? order by tjrq desc limit 1";
+		List<TB_TJFX_CDZL> list = null;
+		try {
+			//tjrq 选最大的
+			String sql = "select * from tb_tjfx_cdzl where yggh=? order by tjrq desc limit 1";
+			
+			list = jdbcTemplate.query(sql, new Object[] {yggh}
+			,new BeanPropertyRowMapper<TB_TJFX_CDZL>(TB_TJFX_CDZL.class));
+		}catch(Exception e ) {
+			log.error("发生异常："+LogsUtil.getStackTrace(e));
+		}
 		
-		List<TB_TJFX_CDZL> list = jdbcTemplate.query(sql, new Object[] {yggh}
-		,new BeanPropertyRowMapper<TB_TJFX_CDZL>(TB_TJFX_CDZL.class));
 		
 		return list;
 	}
